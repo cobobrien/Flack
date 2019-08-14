9e632da831b19bee17074e3395a3ef310b10e430
cobobrien <ccfobrien@gmail.com>
2019-08-14 20:12:51 +0100

    basically functionality of sockets and AJAX working

--- application.py
+++ application.py
@@ -1,18 +1,34 @@
 import os
 
-from flask import Flask
+from flask import Flask,render_template, jsonify, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+from collections import defaultdict
+channel_messages = defaultdict(list)
 
 @app.route("/")
 def index():
-    x = 56
-    y = x
-    return "Project 2: TODO"
+    return render_template("index.html", channel_messages=channel_messages)
+
+@app.route("/channel", methods=["POST"])
+def channel():
+    channel_name = request.form.get("channel_name")
+    if channel_name in channel_messages:
+        return jsonify({"success": False})
+    else:
+        channel_messages[channel_name] = []
+        return jsonify({"success": True})
+
+@socketio.on("submit message")
+def message(data):
+    channel = data["channel"]
+    channel_messages[channel].append(data)
+    emit("message submitted", channel_messages, broadcast=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
---
+++ static/mainpage.js
@@ -0,0 +1,93 @@
+if (!localStorage.getItem('username'))
+    localStorage.setItem('username', "");
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('#exampleModal').modal('show')
+    document.querySelector('#saveusr').onclick = () => {
+         var name = document.getElementById("usr").value
+         localStorage.setItem('username',name);
+         document.querySelector('#username').innerHTML = `@${name}`;
+    }
+
+    document.addEventListener( "click", channelListener );
+    function channelListener(event){
+        var element = event.target;
+        if(element.classList.contains("list-group-item")){
+            var channel_to_store = element.id;
+            localStorage.setItem('channel',channel_to_store);
+        }
+    }
+
+    document.querySelector('#savechannel').onclick = () => {
+         const a = document.createElement('a');
+         a.innerHTML = document.querySelector('#channel_name').value;
+         a.className = "list-group-item list-group-item-action";
+         a.id = document.querySelector("#channel_name").value;
+         a.href = "#" + document.querySelector("#channel_name").value;
+         a.dataset.toggle = "list";
+         a.role = "tab";
+         a.setAttribute("aria-controls", document.querySelector("#channel_name").value);
+
+          // Initialize new request
+          const request = new XMLHttpRequest();
+          const channel_name =  a.innerHTML;
+          request.open('POST', '/channel');
+
+          // Callback function for when request completes
+          request.onload = () => {
+
+              // Extract JSON data from request
+              const data = JSON.parse(request.responseText);
+
+              // Update the result div
+              if (data.success) {
+                  document.querySelector('#list-tab').append(a);
+              }
+              else {
+                    const alert = document.querySelector('#channel_alert');
+                    alert.style.display = "",
+                    alert.innerHTML = "Channel name already in use. Please pick another name";
+
+              }
+          }
+
+          // Add data to send with request
+          const data = new FormData();
+          data.append('channel_name', channel_name);
+
+          // Send request
+          request.send(data);
+
+    }
+    document.querySelector('#channel_done').onclick = () => {
+        document.querySelector('#channel_name').value = "";
+    }
+
+    // Connect to websocket
+    var socket = io.connect('http://127.0.0.1:5000');
+
+    // When connected, configure buttons
+    socket.on('connect', () => {
+
+        // Each button should emit a "submit vote" event
+        document.querySelector('#message_submit').onclick = () => {
+
+                var message = document.querySelector('#message_send').value;
+                var sender = localStorage.getItem('username');
+                var currentdate = new Date();
+                var channel = localStorage.getItem('channel');
+                socket.emit('submit message', {"message": message, "sender":sender,"timestamp":currentdate, "channel":channel});
+
+        };
+    });
+
+      // When a new vote is announced, add to the unordered list
+    socket.on('message submitted', data => {
+         const p = document.createElement('p');
+         var channel = localStorage.getItem('channel');
+         p.innerHTML = data[channel].pop()["message"];
+         document.querySelector('#messages').append(p);
+    });
+
+});
+
